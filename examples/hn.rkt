3dba01920ba891fdd8a60998958d0d3ef48cdb50
Bogdan Popa <bogdan@defn.io>
2021-06-22 12:05:55 +0300

    examples: clean up hn example

--- examples/hn.rkt
+++ examples/hn.rkt
@@ -9,7 +9,32 @@
          racket/format
          racket/gui/easy
          racket/gui/easy/operator
-         racket/list)
+         racket/list
+         racket/match)
+
+;; Data ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(struct story (id author title url score text comment-ids comments) #:transparent)
+(struct comment (id author text deleted?) #:transparent)
+
+(define (hash->story h)
+  (story
+   (hash-ref h 'id)
+   (hash-ref h 'by)
+   (hash-ref h 'title)
+   (hash-ref h 'url #f)
+   (hash-ref h 'score 0)
+   (hash-ref h 'text #f)
+   (hash-ref h 'kids null)
+   null))
+
+(define (hash->comment h)
+  (comment
+   (hash-ref h 'id)
+   (hash-ref h 'by "anon")
+   (hash-ref h 'text "")
+   (hash-ref h 'deleted #f)))
+
 
 ;; API ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -49,23 +74,125 @@
   (get-items ids concurrency))
 
 (define (get-top-stories limit [concurrency 20])
-  (get-stories "topstories" limit concurrency))
+  (map hash->story (get-stories "topstories" limit concurrency)))
 
 (define (get-new-stories limit [concurrency 20])
-  (get-stories "newstories" limit concurrency))
+  (map hash->story (get-stories "newstories" limit concurrency)))
 
-(define (load-children it [concurrency 20])
-  (hash-set it 'children (get-items (hash-ref it 'kids null) concurrency)))
+(define (load-comments s [concurrency 20])
+  (struct-copy story s [comments (map hash->comment (get-items (story-comment-ids s) concurrency))]))
+
+(define (visit it)
+  (cond
+    [(and (story? it) (story-url it)) => send-url]
+    [(story? it) (send-url (format "https://news.ycombinator.com/item?id=~a" (story-id it)))]
+    [else (send-url (format "https://news.ycombinator.com/item?id=~a" (comment-id it)))]))
+
+
+;; Helpers and Widgets ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define title-font (font "SF Pro" 18 #:weight 'medium))
+(define subtitle-font (font "SF Pro" 14 #:weight 'medium))
+(define regular-font (font "SF Pro" 12 #:weight 'medium))
+(define title-color (color "black"))
+(define highlight-color (color "blue"))
+(define points-color (color "orange"))
+(define regular-color (color #x50 #x50 #x50))
+(define inverted-color (color "white"))
+
+;; A custom view<%> to display a list of picts.
+(define pict-list%
+  (class* object% (view<%>)
+    (init-field @items item->pict item-height action)
+    (super-new)
+
+    (define/public (dependencies)
+      (list @items))
+
+    (define/public (create parent)
+      (new cl:canvas-list%
+           [parent parent]
+           [hover-color (color #xF9 #xF9 #xF9)]
+           [selection-color (color #x00 #x00 #xFF)]
+           [items (obs-peek @items)]
+           [item-height item-height]
+           [paint-item-callback (λ (_self item state dc _w _h)
+                                  (p:draw-pict (item->pict item state) dc 10 10))]
+           [action-callback (λ (_self item _event)
+                              (action item))]))
+
+    (define/public (update v what val)
+      (when (eq? what @items)
+        (send v set-items val)))
+
+    (define/public (destroy _v)
+      (void))))
+
+(define (pict-list @items item->pict action
+                   #:item-height item-height)
+  (new pict-list%
+       [@items @items]
+       [item->pict item->pict]
+       [item-height item-height]
+       [action action]))
+
+(define pict
+  (make-keyword-procedure
+   (λ (kws kw-args @data make-pict . args)
+     (define (draw dc v)
+       (p:draw-pict (make-pict v) dc 0 0))
+     (keyword-apply canvas kws kw-args @data draw args))))
+
+(define (styled-text t)
+  (apply p:hbl-append (for/list ([t (in-list t)])
+                        (match t
+                          [(? string? text) (p:colorize (p:text text regular-font) regular-color)]
+                          [`(,color ,text) (p:colorize (p:text text regular-font) color)]
+                          [`(,font ,color ,text) (p:colorize (p:text text font) color)]))))
+
+(define (story-pict s)
+  (p:vl-append
+   5
+   (styled-text `((,title-font ,title-color ,(story-title s))))
+   (styled-text `((,subtitle-font ,highlight-color ,(or (story-url s) "show"))))
+   (styled-text `((,points-color ,(~a (story-score s)))
+                  " points by "
+                  (,highlight-color ,(story-author s))))))
+
+(define (comment-pict c state)
+  (define username (comment-author c))
+  (p:vl-append
+   5
+   (p:lt-superimpose
+    (p:dc
+     (λ (dc dx dy)
+       (define old-brush (send dc get-brush))
+       (define old-pen (send dc get-pen))
+       (define-values (w h _d _v)
+         (send dc get-text-extent username regular-font))
+       (send dc set-smoothing 'smoothed)
+       (send dc set-pen "black" 0 'transparent)
+       (send dc set-brush inverted-color 'solid)
+       (send dc draw-rounded-rectangle dx dy (+ w 8) (+ h 4) 5)
+       (send dc set-pen old-pen)
+       (send dc set-brush old-brush))
+     0 0)
+    (p:inset
+     (styled-text `((,points-color ,(comment-author c))))
+     4 2))
+   (styled-text
+    `((,(case state
+          [(selected) inverted-color]
+          [else regular-color])
+       ,(comment-text c))))))
 
 
 ;; GUI ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; either '(overview) or '(story data)
 (define @mode (@ '(overview)))
-(define @story (@mode . ~> . (λ (m)
-                               (case (car m)
-                                 [(story) (cadr m)]
-                                 [else #f]))))
+(define @story (@mode . ~> . (match-lambda
+                               [`(story ,s) s]
+                               [_ #f])))
 
 (define @sections (@ #(top new)))
 (define @section (@ 'top))
@@ -106,148 +233,68 @@
               (@new-stories . := . null)
               (async (@new-stories . := . (get-new-stories 50)))])]))))))
 
-(define (story-list @mode @stories)
+(define (story-list @m @ss)
   (hpanel
    (cond/view
-    [(@stories . ~> . null?)
+    [(@ss . ~> . null?)
      (text "Loading...")]
 
     [else
      (table
       '("Title" "Score" "By")
-      (@stories . ~> . list->vector)
+      (@ss . ~> . list->vector)
       #:entry->row (λ (s)
-                     (vector (hash-ref s 'title)
-                             (~a (hash-ref s 'score))
-                             (~a (hash-ref s 'by))))
+                     (vector (story-title s)
+                             (~a (story-score s))
+                             (~a (story-author s))))
       (λ (event stories index)
         (case event
           [(dclick)
-           (define story (vector-ref stories index))
-           (@mode . := . `(story ,story))
-           (async (@mode . := . `(story ,(load-children story))))])))])))
+           (define s (vector-ref stories index))
+           (@m . := . `(story ,s))
+           (async (@m . := . `(story ,(load-comments s))))])))])))
 
-(define title-font (font "SF Pro" 18 #:weight 'medium))
-(define subtitle-font (font "SF Pro" 14 #:weight 'medium))
-(define regular-font (font "SF Pro" 12 #:weight 'medium))
-(define highlight-color (color "blue"))
-(define points-color (color "orange"))
-(define regular-color (color 80 80 80))
-(define inverted-color (color "white"))
+(define (story-view @m @s)
+  (vpanel
+   #:alignment '(left top)
+   (hpanel
+    #:stretch '(#t #f)
+    (button "Back" (λ () (@m . := . '(overview))))
+    (spacer)
+    (button "Visit" (λ () (visit (obs-peek @s)))))
+   (vpanel
+    (pict
+     #:style '(transparent)
+     #:stretch '(#t #f)
+     #:min-size '(#f 90)
+     @s
+     (λ (s)
+       (p:inset (story-pict s) 10 10)))
+    (pict-list
+     (@s . ~> . (λ (s) (if s (filter-not comment-deleted? (story-comments s)) null)))
+     #:item-height 60
+     (λ (c state)
+       (comment-pict c state))
+     (λ (c)
+       (visit c))))))
 
-(define (story-pict s)
-  (p:vl-append
-   5
-   (p:text (hash-ref s 'title) title-font)
-   (p:colorize (p:text (hash-ref s 'url "show") subtitle-font) highlight-color)
-   (p:hc-append
-    5
-    (p:colorize (p:text (number->string (hash-ref s 'score)) regular-font) points-color)
-    (p:colorize (p:text "points by" regular-font) regular-color)
-    (p:colorize (p:text (hash-ref s 'by) regular-font) highlight-color))))
+(define app
+  (window
+   #:title "Hacker News"
+   #:size '(800 600)
+   (if/view @story
+            (story-view @mode @story)
+            (hpanel
+             #:alignment '(left top)
+             section-list
+             (story-list
+              @mode
+              (obs-combine
+               (λ (section top-stories new-stories)
+                 (case section
+                   [(top) top-stories]
+                   [(new) new-stories]))
+               @section @top-stories @new-stories))))))
 
-(define (comment-pict c state)
-  (define username (hash-ref c 'by "anon"))
-  (p:vl-append
-   5
-   (p:lt-superimpose
-    (p:dc
-     (λ (dc dx dy)
-       (define old-brush (send dc get-brush))
-       (define old-pen (send dc get-pen))
-       (define-values (w h _d _v)
-         (send dc get-text-extent username regular-font))
-       (send dc set-smoothing 'smoothed)
-       (send dc set-pen "black" 0 'transparent)
-       (send dc set-brush inverted-color 'solid)
-       (send dc draw-rounded-rectangle dx dy (+ w 8) (+ h 4) 5)
-       (send dc set-pen old-pen)
-       (send dc set-brush old-brush))
-     0 0)
-    (p:inset
-     (p:colorize (p:text (hash-ref c 'by "anon") regular-font) points-color)
-     4 2))
-   (p:colorize
-    (p:text (hash-ref c 'text) regular-font)
-    (case state
-      [(selected) inverted-color]
-      [else regular-color]))))
-
-(define canvas-list%
-  (class* object% (view<%>)
-    (init-field @items item->pict item-height)
-    (super-new)
-
-    (define/public (dependencies)
-      (list @items))
-
-    (define/public (create parent)
-      (new cl:canvas-list%
-           [parent parent]
-           [hover-color (color #xF9 #xF9 #xF9)]
-           [selection-color (color #x00 #x00 #xFF)]
-           [items (obs-peek @items)]
-           [item-height item-height]
-           [paint-item-callback (λ (_self item state dc _w _h)
-                                  (p:draw-pict (item->pict item state) dc 10 10))]))
-
-    (define/public (update v what val)
-      (when (eq? what @items)
-        (send v set-items val)))
-
-    (define/public (destroy _v)
-      (void))))
-
-(define (canvas-list @items item->pict
-                     #:item-height item-height)
-  (new canvas-list%
-       [@items @items]
-       [item->pict item->pict]
-       [item-height item-height]))
-
-(render
- (window
-  #:title "Hacker News"
-  #:size '(800 600)
-  (cond/view
-   [@story
-    (vpanel
-     #:alignment '(left top)
-     (hpanel
-      #:stretch '(#t #f)
-      (button "Back" (λ ()
-                       (@mode . := . '(overview))))
-      (spacer)
-      (button "Visit" (λ ()
-                        (send-url (hash-ref (obs-peek @story) 'url)))))
-     (vpanel
-      (canvas
-       #:style '(transparent)
-       #:stretch '(#t #f)
-       #:min-size '(#f 90)
-       @story
-       (λ (dc story)
-         (send dc set-smoothing 'smoothed)
-         (p:draw-pict (story-pict story) dc 10 10)))
-      (canvas-list
-       (@story . ~> . (λ (story)
-                        (let ([story (or story (hash))])
-                          (for/list ([comment (in-list (hash-ref story 'children null))]
-                                     #:unless (hash-ref comment 'deleted #f))
-                            comment))))
-       #:item-height 60
-       (λ (c state)
-         (comment-pict c state)))))]
-
-   [else
-    (hpanel
-     #:alignment '(left top)
-     section-list
-     (story-list
-      @mode
-      (obs-combine
-       (λ (section top-stories new-stories)
-         (case section
-           [(top) top-stories]
-           [(new) new-stories]))
-       @section @top-stories @new-stories)))])))
+(module+ main
+  (render app))
