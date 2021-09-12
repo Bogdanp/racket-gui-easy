fefddaab8db015b0c448d9c2cf0d263edf080e34
Bogdan Popa <bogdan@defn.io>
2021-09-12 14:56:23 +0300

    debugger: make max changes configurable

--- gui-easy-lib/gui/easy/debugger.rkt
+++ gui-easy-lib/gui/easy/debugger.rkt
@@ -40,40 +40,55 @@
   (λ ()
     (channel-put stop-ch #t)))
 
+(struct state (max-changes changes))
+
 (define (start-debugger)
-  (define @state (@ null))
+  (define @state (@ (state 20 null)))
   (define stop-collector-thd
     (start-collector-thd
      (lambda (obs before after)
        (unless (or (equal? obs @state)
                    (obs-derived? obs))
-         (@state . <~ . (λ (state)
-                          (keep (cons (list (current-seconds) obs before after) state) 100)))))))
+         (@state . <~ . (λ (s)
+                          (match-define (state max-changes changes) s)
+                          (define change (list (current-seconds) obs before after))
+                          (struct-copy state s [changes (keep (cons change changes) max-changes)])))))))
   (parameterize ([gui:current-eventspace (gui:make-eventspace)])
     (render
      (window
       #:title "Debugger"
       #:size '(400 600)
       #:mixin (make-debugger-window-mixin stop-collector-thd)
-      (table
-       '("Timestamp" "Observable" "State")
-       #:column-widths '((0 140)
-                         (1 70)
-                         (2 200))
-       (@state . ~> . list->vector)
-       #:entry->row (λ (entry)
-                      (match-define (list ts obs _before after) entry)
-                      (vector
-                       (parameterize ([date-display-format 'iso-8601])
-                         (date->string (seconds->date ts) #t))
-                       (~a (obs-name obs))
-                       (~label after)))
-       (lambda (event entries selection)
-         (case event
-           [(dclick)
-            (match-define (list _ts obs _before after)
-              (vector-ref entries selection) )
-            (obs . := . after)])))))))
+      (vpanel
+       (hpanel
+        #:stretch '(#t #f)
+        (text "Keep:")
+        (input
+         (@state . ~> . (match-lambda
+                          [(state #f _) ""]
+                          [(state n  _) (number->string n)]))
+         (λ (_event text)
+           (@state . <~ . (λ (s)
+                            (struct-copy state s [max-changes (string->number text)]))))))
+       (table
+        '("Timestamp" "Observable" "State")
+        #:column-widths '((0 140)
+                          (1 70)
+                          (2 200))
+        (@state . ~> . (compose1 list->vector state-changes))
+        #:entry->row (λ (entry)
+                       (match-define (list ts obs _before after) entry)
+                       (vector
+                        (parameterize ([date-display-format 'iso-8601])
+                          (date->string (seconds->date ts) #t))
+                        (~a (obs-name obs))
+                        (~label after)))
+        (lambda (event entries selection)
+          (case event
+            [(dclick)
+             (match-define (list _ts obs _before after)
+               (vector-ref entries selection) )
+             (obs . := . after)]))))))))
 
 (define (~label s)
   (~e #:max-width 100 s))
