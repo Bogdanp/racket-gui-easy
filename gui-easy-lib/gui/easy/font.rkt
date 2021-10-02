5d4e6892e2d85cb9476cd59d9cd31b2001c61105
Bogdan Popa <bogdan@defn.io>
2021-10-02 16:00:26 +0300

    font: add font*

--- gui-easy-lib/gui/easy/font.rkt
+++ gui-easy-lib/gui/easy/font.rkt
@@ -2,19 +2,25 @@
 
 (require racket/class
          racket/contract
-         (prefix-in gui: racket/gui))
+         racket/draw
+         (prefix-in gui: racket/gui)
+         racket/string)
 
 (provide
  (contract-out
-  [font (->* (string? (real-in 0 1024.0))
-             (#:family gui:font-family/c
-              #:style gui:font-style/c
-              #:weight gui:font-weight/c
-              #:underline? any/c
-              #:smoothing gui:font-smoothing/c
-              #:hinting gui:font-hinting/c
-              #:size-in-pixels? any/c)
-             (is-a?/c gui:font%))]))
+  [font (font/c string?)]
+  [font* (font/c (or/c string? (listof string?)))]))
+
+(define (font/c name/c)
+  (->* (name/c (real-in 0 1024.0))
+       (#:family gui:font-family/c
+        #:style gui:font-style/c
+        #:weight gui:font-weight/c
+        #:underline? any/c
+        #:smoothing gui:font-smoothing/c
+        #:hinting gui:font-hinting/c
+        #:size-in-pixels? any/c)
+       (is-a?/c gui:font%)))
 
 (define (font face size
               #:family [family 'default]
@@ -25,3 +31,14 @@
               #:hinting [hinting 'aligned]
               #:size-in-pixels? [pixels? #f])
   (send gui:the-font-list find-or-create-font size face family style weight underline? smoothing pixels? hinting))
+
+(define font*
+  (make-keyword-procedure
+   (lambda (kws kw-args faces size)
+     (let ([faces (if (string? faces)
+                      (map string-trim (string-split faces ","))
+                      faces)])
+       (define installed-faces (map string-downcase (get-face-list)))
+       (for/first ([face (in-list faces)]
+                   #:when (member (string-downcase face) installed-faces string=?))
+         (keyword-apply font kws kw-args face size null))))))
--- gui-easy-lib/info.rkt
+++ gui-easy-lib/info.rkt
@@ -4,6 +4,7 @@
 (define collection "racket")
 (define deps '("base"
                "box-extra-lib"
+               "draw-lib"
                "gui-lib"
                "pict-lib"))
 (define build-deps '("rackunit-lib"))
