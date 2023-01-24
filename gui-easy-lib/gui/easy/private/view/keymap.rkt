e0e0002de9d2fef2bda2bbf71bcf1f607d237a1b
Bogdan Popa <bogdan@defn.io>
2023-01-24 06:14:33 +0200

    keymap: add {backward,forward}-word
    Closes #30

--- gui-easy-lib/gui/easy/private/view/keymap.rkt
+++ gui-easy-lib/gui/easy/private/view/keymap.rkt
@@ -1,8 +1,8 @@
 #lang racket/base
 
-(require racket/class
-         (prefix-in gui: racket/gui)
-         racket/match)
+(require (for-syntax racket/base)
+         racket/class
+         (prefix-in gui: racket/gui))
 
 (provide
  the-default-keymap)
@@ -19,29 +19,72 @@
 (define (goto-end editor _event)
   (send editor set-position (send editor last-position)))
 
-(define mappings
-  (case (system-type 'os)
-    [(macosx)
-     `(("c:a" goto-start ,goto-start)
-       ("c:e" goto-end   ,goto-end)
-       ("d:z" undo       ,(make-editor-operation 'undo))
-       ("d:Z" redo       ,(make-editor-operation 'redo))
-       ("d:a" select-all ,(make-editor-operation 'select-all))
-       ("d:x" cut        ,(make-editor-operation 'cut))
-       ("d:c" copy       ,(make-editor-operation 'copy))
-       ("d:v" paste      ,(make-editor-operation 'paste)))]
-    [else
-     `(("home" goto-start ,goto-start)
-       ("end"  goto-end   ,goto-end)
-       ("c:z"  undo       ,(make-editor-operation 'undo))
-       ("c:Z"  redo       ,(make-editor-operation 'redo))
-       ("c:a"  select-all ,(make-editor-operation 'select-all))
-       ("c:x"  cut        ,(make-editor-operation 'cut))
-       ("c:c"  copy       ,(make-editor-operation 'copy))
-       ("c:v"  paste      ,(make-editor-operation 'paste)))]))
+(define (forward-word editor _event)
+  (define pos (box (send editor get-end-position)))
+  (send editor find-wordbreak #f pos 'caret)
+  (send editor set-position (unbox pos)))
 
-(for ([mapping (in-list mappings)])
-  (match-define (list binding name proc) mapping)
-  (define name-str (symbol->string name))
-  (send the-default-keymap add-function name-str proc)
-  (send the-default-keymap map-function binding name-str))
+(define (backward-word editor _event)
+  (define pos (box (send editor get-start-position)))
+  (send editor find-wordbreak pos #f 'caret)
+  (send editor set-position (unbox pos)))
+
+(define (delete-word-backward editor _event)
+  (define start-pos (send editor get-start-position))
+  (define end-pos
+    (let ([b (box start-pos)])
+      (send editor find-wordbreak b #f 'caret)
+      (unbox b)))
+  (unless (= start-pos end-pos)
+    (send editor delete end-pos start-pos)))
+
+(define-syntax (define-proc stx)
+  (syntax-case stx ()
+    [(_ id) #'(define-proc id id)]
+    [(_ id e) #'(send the-default-keymap add-function (symbol->string 'id) e)]))
+
+(define-syntax-rule (define-procs binder ...)
+  (begin (define-proc . binder) ...))
+
+(define-syntax-rule (bind [binding id] ...)
+  (begin (send the-default-keymap map-function binding (symbol->string 'id)) ...))
+
+(define-procs
+  [backward-word]
+  [copy (make-editor-operation 'copy)]
+  [cut (make-editor-operation 'cut)]
+  [delete-word-backward]
+  [forward-word]
+  [goto-end]
+  [goto-start]
+  [paste (make-editor-operation 'paste)]
+  [redo (make-editor-operation 'redo)]
+  [select-all (make-editor-operation 'select-all)]
+  [undo (make-editor-operation 'undo)])
+
+(case (system-type 'os)
+  [(macosx)
+   (bind
+    ["a:backspace" delete-word-backward]
+    ["a:left"      backward-word]
+    ["a:right"     forward-word]
+    ["c:a"         goto-start]
+    ["c:e"         goto-end]
+    ["d:Z"         redo]
+    ["d:c"         copy]
+    ["d:v"         paste]
+    ["d:x"         cut]
+    ["d:z"         undo])]
+  [else
+   (bind
+    ["c:Z"         redo]
+    ["c:a"         select-all]
+    ["c:backspace" delete-word-backward]
+    ["c:c"         copy]
+    ["c:left"      backward-word]
+    ["c:right"     forward-word]
+    ["c:v"         paste]
+    ["c:x"         cut]
+    ["c:z"         undo]
+    ["end"         goto-end]
+    ["home"        goto-start])])
