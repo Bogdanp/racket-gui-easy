d0af56d28bba96f19a80f56135b6281ce12c108f
Bogdan Popa <bogdan@defn.io>
2023-08-12 13:57:11 +0100

    color: support fixnum arguments

--- gui-easy-lib/gui/easy/color.rkt
+++ gui-easy-lib/gui/easy/color.rkt
@@ -2,14 +2,27 @@
 
 (require racket/class
          racket/contract
+         racket/fixnum
          (prefix-in gui: racket/gui))
 
 (provide
  (contract-out
   [color (case->
-          (-> string? (is-a?/c gui:color%))
+          (-> (or/c fixnum? string?) (is-a?/c gui:color%))
           (-> byte? byte? byte? (is-a?/c gui:color%))
           (-> byte? byte? byte? (real-in 0 1.0) (is-a?/c gui:color%)))]))
 
-(define (color . args)
-  (apply make-object gui:color% args))
+(define color
+  (case-lambda
+    [(color)
+     (if (fixnum? color)
+         (make-object gui:color%
+                      (fxand (fxrshift color 24) #xFF)
+                      (fxand (fxrshift color 16) #xFF)
+                      (fxand (fxrshift color 8) #xFF)
+                      (/ (fxand color #xFF) 255.0))
+         (make-object gui:color% color))]
+    [(r g b)
+     (make-object gui:color% r g b)]
+    [(r g b a)
+     (make-object gui:color% r g b a)]))
