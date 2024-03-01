a5426d6b5791c63023a2aac025df059680778e1a
Bogdan Popa <bogdan@defn.io>
2024-03-01 14:09:29 +0200

    build: bump version

--- gui-easy-lib/info.rkt
+++ gui-easy-lib/info.rkt
@@ -1,11 +1,13 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "0.17.1")
+(define version "0.18")
 (define collection "racket")
-(define deps '("base"
-               "box-extra-lib"
-               "draw-lib"
-               "gui-lib"
-               "pict-lib"))
-(define build-deps '("rackunit-lib"))
+(define deps
+  '("base"
+    "box-extra-lib"
+    "draw-lib"
+    "gui-lib"
+    "pict-lib"))
+(define build-deps
+  '("rackunit-lib"))
