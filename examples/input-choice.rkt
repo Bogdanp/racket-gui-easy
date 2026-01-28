5199268a5c9659031bcf8aae82492aafbaafdb0b
Bogdan Popa <bogdan@defn.io>
2026-01-28 07:08:06 +0200

    examples,input-choice: drop unused import

--- examples/input-choice.rkt
+++ examples/input-choice.rkt
@@ -1,15 +1,12 @@
-#lang racket
-(require racket/gui/easy
-         racket/gui/base)
+#lang racket/gui/easy
 
 (define workday-hours '("08:00" "10:00" "12:00" "14:00" "16:00"))
 (define extended-hours (append '("04:00" "06:00")
                                workday-hours
                                '("18:00" "20:00")))
 
-(define @suggested-hours (obs workday-hours))
-
-(define @validated (obs ""))
+(define/obs @suggested-hours workday-hours)
+(define/obs @validated "")
 
 (render
  (window
@@ -24,7 +21,7 @@
   (input #:label "Appointment:"
          (car workday-hours)
          #:choices @suggested-hours
-         (lambda (e str)
+         (lambda (_ str)
            (obs-set! @validated (if (and (regexp-match? #rx"^[0-9][0-9]:[0-9][0-9]$" str)
                                          (and (<= 0 (string->number (substring str 0 2)) 23)
                                               (<= 0 (string->number (substring str 3 5)) 59)))
